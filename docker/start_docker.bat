9e990e764661322af1854ee31247028df7a5d32f
Damian Sawera <damian.sawera@unit4.com>
2023-08-03 08:27:19 +0200

    fixed docker and jenkins

--- docker/start_docker.bat
+++ docker/start_docker.bat
@@ -1,2 +1,6 @@
-cd C:\Users\48500\Desktop\Projekty\OrangeHRM-Testing\docker
+set "PROJECT_DIR=%~dp0"
+set "DOCKER_DIR=%PROJECT_DIR%"
+set "APP_PATH=%DOCKER_DIR%\app"
+cd %DOCKER_DIR%
 docker-compose up
+
